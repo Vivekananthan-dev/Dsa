e2e1aaa5dfd293966bd3e0e61d5ee585bba8bc5f
DevilMasterV <vivekananthan.developer@gmail.com>
2026-04-22 21:15:22 +0530

    Longestcommonprefix

---
+++ ArraysSrting/longestcomprefix.py
@@ -0,0 +1,15 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if prefix == "":
+                return ""
+    return prefix
+
+words = ["flower", "flow", "flight"]
+print(longest_common_prefix(words)) 
