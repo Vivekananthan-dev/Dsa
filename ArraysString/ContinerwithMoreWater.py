42b25ae59809d0fbe6d53bfbcee68dc99cab22ca
DevilMasterV <vivekananthan.developer@gmail.com>
2026-04-28 21:26:56 +0530

    ContinerWithMoreWater

---
+++ ArraysString/ContinerwithMoreWater.py
@@ -0,0 +1,18 @@
+def max_area(height):
+    left, right = 0, len(height) - 1
+    max_water = 0
+    
+    while left < right:
+        width = right - left
+        h = min(height[left], height[right])
+        max_water = max(max_water, width * h)
+        
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    
+    return max_water
+
+print(max_area([1,8,6,2,5,4,8,3,7]))  
+print(max_area([1,1]))                
