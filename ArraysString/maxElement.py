45759a9e638fb83d6f5c86d2aec28273e0faa20e
DevilMasterV <vivekananthan.developer@gmail.com>
2026-04-28 21:24:09 +0530

    MximumElement

---
+++ ArraysString/maxElement.py
@@ -0,0 +1,15 @@
+def majority_element(nums):
+    candidate = None
+    count = 0
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    
+    if nums.count(candidate) > len(nums)//2:
+        return candidate
+    return None
+
+# Example usage
+print(majority_element([3,3,4,2,3,3,5])) 
+print(majority_element([2,2,1,1,1,2,2])) 
